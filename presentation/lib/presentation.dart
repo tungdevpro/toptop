bd8d4484857ab227a1b6a96dc06a457f495d7870
tungdevpro <tungdo21899@gmail.com>
2023-12-15 11:31:33 +0700

    feat: add statusbar color

--- presentation/lib/presentation.dart
+++ presentation/lib/presentation.dart
@@ -2,10 +2,13 @@ library presentation;
 
 import 'package:data/data.dart';
 import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:presentation/di/locator.dart';
 
 class Presentation {
-  static void init() {
+  static void init() async {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     Data.init();
     Domain.init();
     configureDependencies();
